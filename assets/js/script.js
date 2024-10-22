10936be1c817467427938e9a28b488ce0a459f06
Wesley de Oliveira <wesleysistemaor@gmail.com>
2024-10-22 19:20:19 -0300

    initProject_pizzaria

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+// executa um bloco de código ou funções, quando o scroll é feito na janela.
+function none() {
+    var posicaoAtual = document.documentElement.scrollTop;
+
+    if (posicaoAtual >= 350) { 
+        document.getElementById("btn-topo").style.display = "flex";
+    } else {
+        document.getElementById("btn-topo").style.display = "none";
+            
+    }
+
+    if (posicaoAtual > 350) { 
+        document.getElementById("nav-color").style.display = "none"; 
+    } else {
+        document.getElementById("nav-color").style.display = "flex"; 
+        
+    }
+}
+
+// Quando ocorre o scroll
+window.onscroll = function(oEvent) {
+    none();
+}
+
+$(document).ready(function() {
+
+    $("#btn-topo").click(function() {
+        var posicao = $("#home").offset().top;
+        $("html, body").animate({ scrollTop: posicao }, 500);
+    });
+
+    $("#item-home").click(function(event) {
+        event.preventDefault()
+        var posicao = $("#home").offset().top;
+        $("html, body").animate({ scrollTop: posicao }, 500);
+    });
+
+    $("#item-servicos").click(function() {
+        var posicao = $("#servicos").offset().top;
+        $("html, body").animate({ scrollTop: posicao }, 500);
+    });
+
+    $("#item-planos").click(function() {
+        var posicao = $("#planos").offset().top;
+        $("html, body").animate({ scrollTop: posicao }, 500);
+    });
+
+    $("#item-contato").click(function() {
+        var posicao = $("#contato").offset().top;
+        $("html, body").animate({ scrollTop: posicao }, 500);
+    });
+
+    document.getElementById('planCard').addEventListener('click', function() {
+        window.location.href = 'https://datasetsistemas.com.br/DataFood/DeGustPizzaria/#/';
+    });
+    document.getElementById('planCard2').addEventListener('click', function() {
+        window.location.href = 'https://datasetsistemas.com.br/DataFood/DeGustPizzaria/#/';
+    });
+    document.getElementById('planCard3').addEventListener('click', function() {
+        window.location.href = 'https://datasetsistemas.com.br/DataFood/DeGustPizzaria/#/';
+    });
+});
